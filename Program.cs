d7072bee36b1ec6d75f6d9e3c99173af88f38153
John J Kauflin <johnkauflin@gmail.com>
2026-02-16 15:17:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mp3Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
